8c5d71f25a770c2ff210796cbf77d4d6eb3160a9
astroash <astroaisha@gmail.com>
2017-11-01 21:01:29 +0000

    Add placeholder to show input updates state

--- src/Routes/Home.elm
+++ src/Routes/Home.elm
@@ -11,5 +11,5 @@ home model =
     div [ class "w-60-ns center" ]
         [ h1 [ class "tc f1" ] [ text "hello" ]
         , p [ class "f3 w60 mh1 tc" ] [ text "Welcome to this SPA elm boiler plate. Tachyons is included. Go nuts." ]
-        , input [ class "f3 w30 pa1 center db ba", onInput Change, value model.userInput ] []
+        , input [ class "f3 w30 pa1 center db ba tc", onInput Change, value model.userInput, placeholder "I update model, yo" ] []
         ]
