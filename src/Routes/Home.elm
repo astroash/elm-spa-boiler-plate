577551303be460476ae3ab7783f2444738bc82a1
astroash <astroaisha@gmail.com>
2017-11-02 07:35:16 +0000

    Edit placeholder

--- src/Routes/Home.elm
+++ src/Routes/Home.elm
@@ -11,5 +11,5 @@ home model =
     div [ class "w-60-ns center" ]
         [ h1 [ class "tc f1" ] [ text "hello" ]
         , p [ class "f3 w60 mh1 tc" ] [ text "Welcome to this SPA elm boiler plate. Tachyons is included. Go nuts." ]
-        , input [ class "f3 w30 pa1 center db ba tc", onInput Change, value model.userInput, placeholder "I update model, yo" ] []
+        , input [ class "f3 w30 pa1 center db ba tc", onInput Change, value model.userInput, placeholder "I update state, yo" ] []
         ]
